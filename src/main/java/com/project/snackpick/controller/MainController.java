456276c65e665780580199442f6c66b80bf49910
Audrey <cw195505@gmail.com>
2025-02-04 15:27:15 +0900

    Feat: 로그인 페이지 화면 제작

---
+++ src/main/java/com/project/snackpick/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.project.snackpick.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    /* 메인 페이지 */
+    @GetMapping("/index.html")
+    public String mainPage() {
+        return "index";
+    }
+
+    /* 로그인 페이지 */
+    @GetMapping("/login.html")
+    public String loginPage() {
+       return "user/login";
+    }
+}
