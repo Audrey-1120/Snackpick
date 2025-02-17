952bb2287b1578e9ff52ad00f7150aa807bb6d50
Audrey <cw195505@gmail.com>
2025-02-18 00:02:18 +0900

    Feat: swagger 설정 추가 - SecurityConfig: swagger 관련 url 접근 허용 - SwaggerConfig 파일 생성 - swagger api 버전 업그레이드(2.5.0 -> 2.7.0)

--- src/main/java/com/project/snackpick/config/SecurityConfig.java
+++ src/main/java/com/project/snackpick/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth // 람다식 형태 활용
                         .requestMatchers("/member/**", "/", "/index.page").permitAll() // 로그인, 회원가입, 메인(루트)경로에 대해서는 모든 권한 허용(permitAll())
                         .requestMatchers("/assets/**", "/css/**", "/js/**", "/images/**").permitAll() // 정적 파일 허용
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN") // admin 경로는 ADMIN이라는 권한만 가진 사용자만 접근 가능
                         .anyRequest().authenticated()); // 그외 나머지 요청에서는 로그인한 사용자만 접근 가능
 
