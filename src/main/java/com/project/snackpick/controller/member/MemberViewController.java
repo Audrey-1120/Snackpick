a3015a3c3816945a8f2742e3a53e5ffe4dde6fc9
Audrey <cw195505@gmail.com>
2025-03-25 00:18:10 +0900

    Fix: 로그인한 상태에서 로그인 페이지 접속 시 메인 페이지로 이동하는 코드 추가

--- src/main/java/com/project/snackpick/controller/member/MemberViewController.java
+++ src/main/java/com/project/snackpick/controller/member/MemberViewController.java
@@ -1,6 +1,7 @@
 package com.project.snackpick.controller.member;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,12 @@ public class MemberViewController {
     // 로그인 페이지
     @GetMapping("/login.page")
     @Operation(summary = "로그인 페이지 이동", description = "로그인 페이지로 이동")
-    public String loginPage() {
-       return "member/login";
+    public String loginPage(Authentication authentication) {
+
+        if(authentication != null && authentication.isAuthenticated()) {
+            return "redirect:/";
+        }
+        return "member/login";
     }
 
     // 회원가입 페이지
