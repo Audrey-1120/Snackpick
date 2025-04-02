453e808ad7fa8f61f017c96c7aac0fcd7157a2d3
Audrey <cw195505@gmail.com>
2025-04-02 23:28:58 +0900

    Feat: 댓글 작성 기능 구현

--- src/main/java/com/project/snackpick/dto/CommentDTO.java
+++ src/main/java/com/project/snackpick/dto/CommentDTO.java
@@ -4,12 +4,14 @@ import com.project.snackpick.entity.CommentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentDTO {
 
     private int commentId, depth, groupId, reviewId;
